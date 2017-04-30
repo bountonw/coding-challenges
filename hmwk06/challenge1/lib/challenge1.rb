929b1d34d84afa0e9124750591e6a34e23688c8e
Brian Wilson <bountonw@gmail.com>
2017-04-30 22:31:45 +0000

    Add instructions to both challenges

--- hmwk06/challenge1/lib/challenge1.rb
+++ hmwk06/challenge1/lib/challenge1.rb
@@ -1,3 +1,27 @@
+# ## Code Challenge 1:  Balanced Parenthesis (i.e. {[( )
+
+# - Write a function which determines whether or not a string of characters are
+#   balanced.  Thus, your Ruby program should return `true` if it's balanced and
+#   `false` otherwise.
+
+#   In order to complete this challenge, all specs must pass and you can run them
+#   by doing the following:
+
+#   ```
+#   $ unzip homework-06.zip
+#   $ cd /path/to/homework-06/challenge1
+#   $ rspec
+#   ```
+
+#   The expected output is as follows:
+
+#   ```
+#   .......
+
+#   Finished in 0.00179 seconds (files took 0.09853 seconds to load)
+#   7 examples, 0 failures
+#   ```
+
 class Challenge1
   def self.balanced?(_string)
     # your implementation goes here
