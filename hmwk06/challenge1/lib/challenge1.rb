ebd7eae02c37269415f4d37e384617b59d8a1ad3
Brian Wilson <bountonw@gmail.com>
2017-05-04 17:26:25 +0000

    Solve challenge1.rb

--- hmwk06/challenge1/lib/challenge1.rb
+++ hmwk06/challenge1/lib/challenge1.rb
@@ -4,26 +4,36 @@
 #   balanced.  Thus, your Ruby program should return `true` if it's balanced and
 #   `false` otherwise.
 
-#   In order to complete this challenge, all specs must pass and you can run them
-#   by doing the following:
-
-#   ```
-#   $ unzip homework-06.zip
-#   $ cd /path/to/homework-06/challenge1
-#   $ rspec
-#   ```
-
-#   The expected output is as follows:
-
-#   ```
-#   .......
-
-#   Finished in 0.00179 seconds (files took 0.09853 seconds to load)
-#   7 examples, 0 failures
-#   ```
-
 class Challenge1
-  def self.balanced?(_string)
-    # your implementation goes here
+  def self.open_enclosures
+    ["(", "[", "{"]
+  end
+
+  def self.close_enclosures
+    [")", "]", "}"]
+  end
+
+  def self.valid_pair?(open, close)
+    return true if open == "(" && close == ")"
+    return true if open == "[" && close == "]"
+    return true if open == "{" && close == "}"
+    false
+  end
+
+  def self.balanced?(string)
+    input_array = string.split('')
+    open_enclosures_array = []
+    input_array.each do |char| 
+      if open_enclosures.include? char
+        open_enclosures_array << char
+      elsif close_enclosures.include? char
+        if open_enclosures_array.empty? || !valid_pair?(open_enclosures_array.last, char)
+          return false
+        else
+          open_enclosures_array.pop
+        end
+      end
+    end
+    open_enclosures_array.empty?
   end
 end
