42a81f97747604ecfd64e6db3b5041f34a0c92b1
Brian Wilson <bountonw@gmail.com>
2017-05-08 18:28:36 +0000

    Preliminary attempts at Linked List challenge

---
+++ LinkedList/stack.rb
@@ -0,0 +1,23 @@
+load 'node.rb'
+
+class Stack
+    attr_accessor :data, :next
+
+    def initialize(data, next=nil)
+        @data = data
+        @next = next_node
+    end
+
+    # Push a value onto the stack
+    def push(value)
+        
+    end
+
+    # Pop an item off the stack.
+    # Remove the last item that was pushed onto the
+    # stack and return the value to the user
+    def pop
+        # I RETURN A VALUE
+    end
+
+end
