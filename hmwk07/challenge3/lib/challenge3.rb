02e0407deb6ef2df2d81145444ef43180f2940cc
Brian Wilson <bountonw@gmail.com>
2017-05-10 18:44:32 +0000

    Preleminary solution for challenge3

--- hmwk07/challenge3/lib/challenge3.rb
+++ hmwk07/challenge3/lib/challenge3.rb
@@ -11,30 +11,16 @@
 #   => "97a122z97a122z97a122z97a122z97a"
 
 #   >> Challenge3.rewrite()
-#   ```
 
-#   In order to complete this challenge, all specs must pass and you can run them
-#   by doing the following:
-
-#   ```
-#   $ unzip homework-07.zip
-#   $ cp /path/to/homework-07/challenge3
-#   $ rspec
-#   ```
-
-#   The expected output is as follows:
-
-#   ```
-#   ...
-
-#   Finished in 0.00194 seconds (files took 0.10175 seconds to load)
-#   3 examples, 0 failures
-#   ```
 
 #   Note: You are not to use regular expressions to complete this challenge.
 
-require_relative '../lib/writable'
+require_relative 'writable'
 
 class Challenge3
-  # write your solution here
+  def self.rewrite(string, function)
+  	input_array = string.split('')
+  	# p input_array
+  	function.call(input_array)
+  end
 end
