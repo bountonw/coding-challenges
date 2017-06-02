2103719034bf9db175f27a876f64d9ed9e0f58ba
Brian Wilson <bountonw@gmail.com>
2017-06-02 11:20:59 -0700

    Revise Comments

--- Luhn/lib/luhn.rb
+++ Luhn/lib/luhn.rb
@@ -1,5 +1,7 @@
+# Luhn algorithm for validating credit card numbers
 
-# Step 1 - break the credit card into it's individual digits.
+# Step 0 - Return false if credit card length does not equal 16
+# Step 1 - Break the credit card into it's individual digits.
 # Step 2 - Start from the right, double every second digit (i.e digit 2, 4, 6 etc.).
 # Step 3 - If the doubled value is greater than or equal to 10, take the value and subtract 9 from it.
 # Step 4 - Sum the digits.
